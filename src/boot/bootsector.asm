1957e9eff987f662873e66ff431fb8578b54d179
Tom Tarpey <tomtarpeydev@gmail.com>
2021-02-21 23:14:35 +0000

    added kernel_load function and loaded some dummy sectors

--- src/boot/bootsector.asm
+++ src/boot/bootsector.asm
@@ -1,32 +1,61 @@
 ; to assemble we can do "nasm -f bin bootsector.asm -o bootsector.bin"
 [org 0x7c00]
+; constants
+KERNEL_OFFSET equ 0x1000 ; TODO: use this value in the linking stage
+
+; setup a stack
+mov bp, 0x9000
+mov sp, bp
+
 ; entry point
 mov bx, MESSAGE_BOOT
 call println
 
+call load_kernel
+
+
+
+
 jmp $
 
 ; include files
-; printing to the screen
 %include "boot/printing.asm"
 %include "boot/disk_tools.asm"
 %include "boot/gdt.asm"
-; TODO: include a 32 Bit printing file here
+%include "boot/print_32bit.asm"
 %include "boot/switch_pm.asm"
 
 
 ; TODO: load kernel function
 [bits 16]
 load_kernel:
-    ; TODO: fill in logic here...
+    pusha
+    ; TODO: fill in more logic here...
+    mov bx, MESSAGE_KERNEL_LOAD
+    call println
+
+    mov bx, KERNEL_OFFSET ; read from disk and store in 0x1000
+    mov dh, 16 ; load lots of sectors in to memory
+    ; TODO: get the boot drive in to dl
+    call disk_load
+    popa
+    ret
+
+
 
 
 [bits 32]
 BEGIN_PM:
     ; TODO: fill in logic here...
-    
+
 ; data
+; TODO: make room to hold the boot drive address
 MESSAGE_BOOT db 'Booting Lambda OS...', 0
+MESSAGE_PM db 'Entered 32 Bit Protected Mode...', 0
+MESSAGE_KERNEL_LOAD db 'Loading Kernel in to Memory', 0
+
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
+
+times 10240 db 0 ; TODO: remove this when we have a kernel
---
+++ src/boot/print_32bit.asm
@@ -0,0 +1,1 @@
+; TODO: logic for pm_print function
