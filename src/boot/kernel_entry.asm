09af8693cbe46d226bfc8410f162666a652dce19
Tom Tarpey <tomtarpeydev@gmail.com>
2021-02-25 23:30:41 +0000

    Back to the roots by calling some assembly functions from our C Kernel

--- src/boot/kernel_entry.asm
+++ src/boot/kernel_entry.asm
@@ -1,4 +1,16 @@
 [bits 32]
+global somefunction
 [extern main] ; defines some calling point
 call main ; this will call our function in kernel.c
 jmp $ ; we should never get here
+
+somefunction:
+    pusha ; store the state of the cpu registers on the stack
+    mov ebx, MESSAGE_ASM
+    call print_pm
+    popa ; restore all of the cpu register state
+    ret
+
+%include "boot/print_32bit.asm"
+
+MESSAGE_ASM db 'This is from Assembly called in C Kernel', 0
--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -4,11 +4,13 @@ void dummy_test_entrypoint() {
 
 char output_char();
 char* output_str();
+extern void somefunction();
 
 void main() {
     char* graphic_mem = (char*) 0xb8000;
-    char x = output_char();
-    char* my_string = output_str();
+    somefunction(); // from Assembly
+    char x = output_char(); // from rust
+    char* my_string = output_str(); // from C++
     *graphic_mem = x;
     int j = 0;
     for (int i = 2; i < 36; i += 2) {
