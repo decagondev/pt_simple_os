6e3699881a30e9fc2f79265875d0f713dded315d
Tom Tarpey <tomtarpeydev@gmail.com>
2021-02-26 00:08:03 +0000

    FIX: i changes return type to u8 in rust to fix warnings

--- src/kernel/rustkern.rs
+++ src/kernel/rustkern.rs
@@ -1,7 +1,7 @@
 #![crate_type = "staticlib"]
-use std::primitive::char;
+// use std::primitive::char; removed as u8 works fine for a char
 
 #[no_mangle]
-fn output_char() -> std::primitive::char {
-    'G' as std::primitive::char
+fn output_char() -> u8 { // changed to u8 to stop warnings and also to clean up the types
+    'X' as u8
 }
